1295ee2aaa4a752cde3cf790bf35c85a9593a3bb
GustaHSR <126812739+GustaHSR@users.noreply.github.com>
2023-04-09 15:40:47 -0300

    Update ListaCircularDinamca.cpp

--- ListaCircularDinamca.cpp
+++ ListaCircularDinamca.cpp
@@ -112,6 +112,8 @@ class ListaCircular
 				cauda = newNo;
 				qtd++;
 			}
+			cauda->setProx(cabeca);
+			cabeca->setAnt(cauda);
 			
 		}
 
@@ -127,7 +129,8 @@ class ListaCircular
 		}
 		void remover(int v){
 			No* c = cabeca;
-			if(!existe(v)){
+			if(vazia()) cout << "Lista vazia!\n" << endl;
+			else if(!existe(v)){
 				cout<<"Valor nao existe na lista! ";
 			}
 			else {
@@ -138,6 +141,8 @@ class ListaCircular
 				if(c->obterAnt()) c->obterAnt()->setProx(c->obterProx());
 				delete c;
 				qtd--;
+				if(qtd == 0) cabeca = NULL;
+				
 			}
 		}
 
@@ -153,27 +158,73 @@ class ListaCircular
 				cout << endl;
 				cout << "Percorrendo lista " << a << " vezes a partir do valor: " << v << endl << endl;
 				int p = 0;
+				sleep(1);
 				while (c->obterValor() != v) {
 					c = c->obterProx();
 				}
-				while (p != a) {
+				while (p < a) {
 					cout << c->obterValor() << " ";
 					c = c->obterProx();
 					if(c->obterValor()==v) p++;
 				}
+				sleep(1);
 			}
 		}
 };
+ListaCircular* l = new ListaCircular();
 int main(){
-    ListaCircular* l = new ListaCircular();
+	system("clear");
+    
     int cases;
     cout << "Digite 1 para inserir algum elemento na lista." << endl;
     cout << "Digite 2 para remover algum elemento da lista." << endl;
     cout << "Digite 3 para ver se existe algum elemento na lista." << endl;
     cout << "Digite 4 para percorrer a lista a partir de um elemento especifico." << endl;
     cout << "Digite 5 para mostrar a lista." << endl;
-    cout << "Digite 0 para encerrar o programa." << endl;
-    
+    cout << "Digite 0 para encerrar o programa.\n" << endl;
 
+	cin >> cases;
+	int a;
+
+	switch(cases) {
+		case 1:
+			cout<< "Qual numero deseja inserir na lista? ";
+			cin>>a;
+			l->inserir(a);
+			cout << "Numero " << a << " inserido na lista com sucesso!" << endl;
+			sleep(1);
+			return main();
+		case 2:
+			cout << "Qual numero deseja remover da lista? ";
+			cin >> a;
+			l->remover(a);
+			if(l->existe(a)) cout <<"\nElemento " << a << " removido com sucesso!" << endl;
+			sleep(1);
+			return main();
+		case 3:
+			cout << "Digite um elemento a ser procurado na lista: ";
+			cin >> a;
+			if(l->existe(a)) cout << "\nO elemento " << a << " está na fila!" << endl;
+			else cout << "\nO elemento não está na lista!" << endl;
+			sleep(1);
+			return main ();
+		case 4:
+			if(l->vazia()) cout << "Lista Vazia!\n";
+			else {
+				l->mostrar();
+				cout << "\nDigite o elemento elemento da lista para começar a percorrer: ";
+				cin >> a;
+				l->percorrer(a);
+				cout << "\nLista percorrida!" << endl;
+				sleep(3);
+			} 
+			return main ();
+		case 5:
+			l->mostrar();
+			sleep(1);
+			return main();
+		case 0:
+			break;		
+	}
     return 0;
 }
